1902f977962bf8aca9036202653aabeed4b4bc1f
tongari <info@tongari-as.com>
2017-07-09 22:40:53 +0900

    assets:precompile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,14 @@ module ApplicationHelper
   end
 
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
+    return image_tag(user.avatar, alt: user.name, class: 'mypage-thumb') if user.avatar?
 
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name)
+    image_tag(img_url, alt: user.name, class: 'mypage-thumb')
   end
 
 end
