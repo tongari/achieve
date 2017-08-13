478adf23e6a9282af01727c52ef91d63858490e2
tongari <info@tongari-as.com>
2017-08-13 22:10:48 +0900

    dive19_1

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
+  resources :conversations do
+    resources :messages
+  end
+
   resources :users, only: [:index, :show]
   resources :relationships, only: [:create, :destroy]
 
